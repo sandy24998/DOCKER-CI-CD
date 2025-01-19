c6af4b9058c6c6e9fe47a85edc2eb682785087a7
Sandeep Pandit <54panditsandeep@gmail.com>
2025-01-19 11:15:08 +0000

    changes done to test github webhook

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, CI/CD with Docker!"
+    return "Hello Guys, CI/CD with Docker!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
